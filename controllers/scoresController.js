5147dabc1a8a8b0d68db13ab4a44bd5a53d7c54a
Dan Collins <dan.collins.dev@gmail.com>
2024-07-05 12:53:35 -0400

    Implemented Posting to the '/scores' endpoint

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -1,5 +1,6 @@
-"use strict"
+"use strict";
 
+const { json } = require("express");
 const fs = require("fs/promises");
 const path = require("path");
 
@@ -7,7 +8,10 @@ const path = require("path");
 const getScores = async (id, resolve, reject) => {
     try {
         // Open the JSON file for the related game
-        const FILE_NAME = path.join(path.dirname(__dirname), `./files/scores/${id}.json`);
+        const FILE_NAME = path.join(
+            path.dirname(__dirname),
+            `./files/scores/${id}.json`
+        );
 
         // Read the file
         const scoreData = await fs.readFile(FILE_NAME);
@@ -17,6 +21,36 @@ const getScores = async (id, resolve, reject) => {
     } catch (error) {
         reject(error);
     }
-}
+};
 
-module.exports = { getScores }
+// Add a new score to a game by id
+const addScore = async (newScore, id, resolve, reject) => {
+    try {
+        // Open the scores file
+        const FILE_NAME = path.join(
+            path.dirname(__dirname),
+            `./files/scores/${id}.json`
+        );
+
+        // Read the file
+        const scoresData = await fs.readFile(FILE_NAME);
+        let scores = JSON.parse(scoresData);
+
+        // Assign the game an id and timestamps for created and last
+        // modified
+        newScore.created_dts = new Date().toUTCString();
+        newScore.last_modified_dts = new Date().toUTCString();
+
+         // Add the new score
+         scores.push(newScore);
+
+        // Writing the new score list to JSON file
+        await fs.writeFile(FILE_NAME, JSON.stringify(scores));
+
+        resolve(newScore);
+    } catch (newScore) {
+        reject(error);
+    }
+};
+
+module.exports = { getScores, addScore };
--- routes/scores.js
+++ routes/scores.js
@@ -1,37 +1,59 @@
-"use strict"
+"use strict";
 
 const express = require("express");
 const router = express.Router();
 const scoresController = require("../controllers/scoresController");
 
-
-// GET request that gets a single game by id
 // Get scores for a game with ID
 router.get("/:id", (req, res, next) => {
-    scoresController.getScores(req.params.id, data => {
-        if (data) {
-            res.status(200).json({
-                status: 200,
-                statusText: "OK",
-                statusMessage: "Scores retrieved.",
-                data: data,
-            });
-
-            
-        } else {
-            res.status(404).json({
-                status: 404,
-                statusText: "Not Found",
-                message: `Scores with id of ${req.params.id} not found`,
-            });
+    scoresController.getScores(
+        req.params.id,
+        (data) => {
+            if (data) {
+                res.status(200).json({
+                    status: 200,
+                    statusText: "OK",
+                    statusMessage: "Scores retrieved.",
+                    data: data,
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    statusText: "Not Found",
+                    message: `Scores with id of ${req.params.id} not found`,
+                });
+            }
+        },
+        (err) => {
+            next(err);
         }
-    }, (err) => {next(err)})
+    );
 });
 
-// POST request for adding a game
-// Send scores for a game with ID
 router.post("/:id", (req, res, next) => {
-    
+    scoresController.addScore(
+        req.body,
+        req.params.id,
+        (data) => {
+            if (data) {
+                res.status(201).json({
+                    status: 201,
+                    statusText: "OK",
+                    statusMessage: "New Score Posted.",
+                    data: data,
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    statusText: "Not Found",
+                    message: `Game with id of ${req.params.id} not found`,
+                });
+            }
+        },
+        (err) => {
+            next(err);
+        }
+    );
 });
 
 module.exports = router;
